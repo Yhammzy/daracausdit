9076fddec3f858d6bccb549f849b8f515210d945
Yhammzy <yisaahmed04@gmail.com>
2022-10-04 01:07:46 +0100

    jkhdhs

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in')
-var x = document.getElementById("myTopnav");
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
 
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
+
 const appearOptions = {
     threshold: 0,
     rootMargin: "0px 0px -60% 0px"
@@ -35,11 +30,3 @@ const appearOnScroll = new IntersectionObserver
     sliders.forEach(slider => {
         appearOnScroll.observe(slider);
     });
-
-    // const hamburger = document.querySelector('.hamburger');
-    // const navLink = document.querySelector('.navmenu');
-    
-    // hamburger.addEventListener('click', () => {
-    //   navLink.classList.toggle('hide');
-    // });
-
